17b40d13b16f7689773fa8a561c53e5ede528af2
BUTiRa030 <zxc63093@gmail.com>
2026-06-23 11:11:09 +0800

    0623Finished

--- server.js
+++ server.js
@@ -13,41 +13,74 @@ const db = new Datastore({
     filename: "score.db",
     autoload: true
 });
+const path = require("path");
+
+console.log(path.resolve("score.db"));
 server.get("/", (req, res) => {
     res.send("API is running");
 });
 server.post("/api/SaveSurvivalTime", (req, res) => {
-    console.log("body =", req.body);
-    console.log("type =", typeof req.body);
+    console.log("收到 SaveSurvivalTime");
+    console.log(req.body);
 
     const score = Number(req.body?.score);
 
     console.log("score =", score);
+    console.log("準備查詢資料庫");
+    console.log("db =", db);
 
     db.findOne({ type: "best" }, (err, doc) => {
 
-        if (!doc) {
-            db.insert({
+    console.log("findOne doc =", doc);
+
+    // 第一次建立紀錄
+    if (!doc) {
+
+        console.log("準備 insert");
+
+        db.insert(
+            {
                 type: "best",
                 score
-            });
+            },
+            (err, newDoc) => {
+                console.log("insert err =", err);
+                console.log("insert doc =", newDoc);
 
-            return res.json({ success: true });
-        }
+                res.json({ success: true });
+            }
+        );
 
-        if (score > doc.score) {
-            db.update(
-                { type: "best" },
-                { $set: { score } },
-                {},
-                () => {
-                    res.json({ success: true });
-                }
-            );
-        } else {
-            res.json({ success: true });
-        }
-    });
+        return;
+    }
+
+    console.log("目前最高分 =", doc.score);
+
+    // 新成績比較高
+    if (score > doc.score) {
+
+        console.log("更新最高分");
+
+        db.update(
+            { type: "best" },
+            { $set: { score: score } },
+            {},
+            (err, numUpdated) => {
+
+                console.log("update err =", err);
+                console.log("numUpdated =", numUpdated);
+
+                res.json({ success: true });
+            }
+        );
+
+    } else {
+
+        console.log("未超過最高分");
+
+        res.json({ success: true });
+    }
+});
 });
 server.get("/api/GetSurvivalTime", (req, res) => {
 
